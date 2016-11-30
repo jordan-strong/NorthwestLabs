ca9ba430892d6bb5c63b6ec9248ad064704772cd
Laura Branham <Laura Branham>
2016-11-29 22:55:33 -0700

    Adding create invoice and view invoice- needs work

---
+++ NorthWestLabs/NorthWestLabs/Controllers/AccountingController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthWestLabs.Controllers
+{
+    public class AccountingController : Controller
+    {
+        // GET: Accounting
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
---
+++ NorthWestLabs/NorthWestLabs/Models/Invoice.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthWestLabs.Models
+{
+    public class Invoice
+    {
+        [Key]
+        [HiddenInput(DisplayValue = false)]
+        [DisplayName("Invoice ID")]
+        public int invoiceID { get; set; }
+
+        [Required(ErrorMessage = "Please enter the total price")]
+        [DisplayName("Total Price of Invoice")]
+        public double totalPrice { get; set; }
+
+        [DisplayName("Payment Terms")]
+        public string paymentTerms { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
+        [DisplayName("Print Date")]
+        public DateTime printDate { get; set; }
+
+        [Required(ErrorMessage = "Please enter a company id")]
+        [DisplayName("Company ID")]
+        public int companyID { get; set; }
+
+        [Required(ErrorMessage = "Please enter an employee ID")]
+        [DisplayName("Employee ID")]
+        public int employeeID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a LT Number")]
+        [DisplayName("LT Number")]
+        [MinLength(6)]
+        [MaxLength(6)]
+        public int LTNum { get; set; }
+
+
+
+
+    }
+}
