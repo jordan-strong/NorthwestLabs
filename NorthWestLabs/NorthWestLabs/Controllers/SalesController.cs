b867496782ec48265be669761837f7fd408aba23
Tyler Mabey <mabeytyler@gmail.com>
2016-11-30 13:20:39 -0700

    added create quote page

---
+++ NorthWestLabs/NorthWestLabs/Controllers/SalesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthWestLabs.Controllers
+{
+    public class SalesController : Controller
+    {
+        // GET: Sales
+        public ActionResult CreateQuote()
+        {
+            return View();
+        }
+
+        public ActionResult QuoteSummary()
+        {
+            return View();
+        }
+    }
+}
---
+++ NorthWestLabs/NorthWestLabs/Models/Sales.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NorthWestLabs.Models
+{
+    public class Sales
+    {
+        [Key]
+        [Required(ErrorMessage = "Please choose an assay")]
+        [DisplayName("Assay requested:")]
+        public string assay { get; set; }
+
+        [Required(ErrorMessage = "Please enter a compound")]
+        [DisplayName("Compound:")]
+        [StringLength(30)]
+        public string compound { get; set; }
+    }
+}
