81243522f3184d099ad9964c7b2ebc1c50eb2267
Alex Cardinal <Alex Cardinal>
2016-11-30 09:36:07 -0700

    alex's stuff

---
+++ NorthwestLabs/NorthWestLabs/Controllers/CRMController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthWestLabs.Controllers
+{
+    public class CRMController : Controller
+    {
+        // GET: CRM
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CRM/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CRM/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // GET: CRM/WorkOrders  This is the new page that was create to show each customer's work orders
+        public ActionResult WorkOrders()
+        {
+            return View();
+        }
+
+        // GET: CRM/Status  This is the new page that was create to show each customer's work orders status
+        public ActionResult Status()
+        {
+            return View();
+        }
+
+        // POST: CRM/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CRM/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CRM/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CRM/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CRM/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ NorthwestLabs/NorthWestLabs/Models/CRM.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+/*This page is the model for the CRM account. It would be a mix of different tables in the database*/
+
+namespace NorthWestLabs.Models
+{
+    public class CRM
+    {
+        [Key]
+        [Required(ErrorMessage = "Please enter a customer ID")]
+        [DisplayName("Customer ID")]
+        public int customerID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a Customer Name")]
+        [DisplayName("Customer")]
+        [StringLength(30)]
+        public string customerName { get; set; }
+
+        [Required(ErrorMessage = "Please enter the billing address")]
+        [DisplayName("Billing Address")]
+        public DateTime billingAddress { get; set; }
+
+        [Required(ErrorMessage = "Please enter the billing city")]
+        [DisplayName("City")]
+        public DateTime city { get; set; }
+
+        [Required(ErrorMessage = "Please enter the billing state")]
+        [DisplayName("State")]
+        public DateTime state { get; set; }
+
+        [Required(ErrorMessage = "Please enter the billing zip code")]
+        [DisplayName("ZIP")]
+        public DateTime ZIP { get; set; }
+
+        [Required(ErrorMessage = "Please enter the contact first name")]
+        [DisplayName("Contact First Name")]
+        public DateTime contactFirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter the contact last name")]
+        [DisplayName("Contact Last Name")]
+        public DateTime contactLastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter the contact phone number")]
+        [DisplayName("Contact Phone Number")]
+        public DateTime contactPhone { get; set; }
+
+        [Required(ErrorMessage = "Please enter the contact email")]
+        [DisplayName("Contact Email")]
+        public DateTime contactEmail { get; set; }
+    }
+
+}
